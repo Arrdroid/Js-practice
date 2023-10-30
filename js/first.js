adde0838299af20f2f91c0b89cff9b48f5008020
Arrdroid <alexmirensko@gmail.com>
2023-10-30 14:50:30 +0200

    initial

---
+++ js/first.js
@@ -0,0 +1,18 @@
+document.body.style.backgroundColor = "black";
+
+/* Practice Chapter 1 Page 26 */
+const bankAccount = 379;
+const phonePrice = 85;
+const phoneAccesory = 12.50;
+const taxRate = 0.12;
+let totalPrice = 0;
+
+function addPhone() {
+  return totalPrice += phonePrice;
+};
+function addAccessory() {
+  return totalPrice += phoneAccesory;
+};
+function payTaxes() {
+  return totalPrice = totalPrice + (totalPrice * taxRate);
+}
