24c7298b2a6607935972368d76d6874d33fba730
Arrdroid <alexmirensko@gmail.com>
2023-11-15 21:10:01 +0300

    update

--- js/objects.js
+++ js/objects.js
@@ -20,4 +20,17 @@ obj["a"]; //"hello world"
 obj["b"]; //42
 obj["c"]; // true
 
-/* Bracket notation is useful if you have a property name with a special characters in it, like obj["hello world!"] - such properties are often referred to as */
+/* Bracket notation is useful if you have a property name with a special characters in it, like obj["hello world!"] - such properties are often referred to as keys when accessed via bracket notation. The [ ] notation requires either a variable or a string (which needs to be wrappe in ".." or '..').   */
+
+/* Also bracket notation is useful if you want to access a property/key but the name is stored in another variable, such as: */
+
+
+let newObj = {
+    a: "hello world",
+    b: 42
+};
+
+let b = "a";
+
+obj[b]; // "hello world"
+obj["a"] // 42
