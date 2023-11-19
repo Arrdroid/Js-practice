c06974bfcc4e53510eb204a658d7fc73011f0871
Arrdroid <alexmirensko@gmail.com>
2023-11-19 17:13:52 +0300

    upd

---
+++ js/arrays.js
@@ -0,0 +1,17 @@
+/* An array is an object that could hold any type of value not particularly in named properties/keys, but rather in numerically indexed positions. */
+
+const arr = [
+    "hello world",
+    42,
+    true
+];
+
+arr[0];     // "hello world"
+arr[1];     // 42
+arr[2];     // true
+
+arr.length; // 3
+
+typeof(arr); // object
+
+/* The best */
